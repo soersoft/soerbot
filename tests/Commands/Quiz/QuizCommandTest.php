8675416ea682e2c433d4856ade06a1642d33e030
Evgeniy Sergeev <esergeev777@yandex.ru>
2019-02-10 08:52:11 +0700

    fix test errors

--- tests/Commands/Quiz/QuizCommandTest.php
+++ tests/Commands/Quiz/QuizCommandTest.php
@@ -18,10 +18,6 @@ class QuizCommandTest extends TestCase
         $registry = $this->createMock('\CharlotteDunois\Livia\CommandRegistry');
         $types = $this->createMock('\CharlotteDunois\Yasmin\Utils\Collection');
 
-        $types->expects($this->exactly(1))->method('has')->willReturn(true);
-        $registry->expects($this->exactly(2))->method('__get')->with('types')->willReturn($types);
-        $this->client->expects($this->exactly(2))->method('__get')->with('registry')->willReturn($registry);
-
         $this->command = $commandCreate($this->client);
 
         parent::setUp();
@@ -33,57 +29,25 @@ class QuizCommandTest extends TestCase
         $this->assertEquals($this->command->groupID, 'games');
     }
 
-    public function testQuizArguments()
-    {
-        $this->assertEquals(sizeof($this->command->args), 1);
-        $this->assertArrayHasKey('key', $this->command->args[0]);
-        $this->assertArrayHasKey('label', $this->command->args[0]);
-        $this->assertArrayHasKey('prompt', $this->command->args[0]);
-        $this->assertArrayHasKey('type', $this->command->args[0]);
-        $this->assertArrayHasKey('validate', $this->command->args[0]);
-
-        $this->assertEquals($this->command->args[0]['key'], 'action');
-        $this->assertEquals($this->command->args[0]['label'], 'action');
-        $this->assertEquals($this->command->args[0]['prompt'], 'quiz [start|add]');
-        $this->assertEquals($this->command->args[0]['type'], 'string');
-    }
-
-    public function testValidateFunctionShouldPassOnlyDefinedActions()
-    {
-        $this->assertTrue($this->command->args[0]['validate']('start'));
-        $this->assertTrue($this->command->args[0]['validate']('add'));
-        $this->assertTrue($this->command->args[0]['validate']('help'));
-
-        $this->assertFalse($this->command->args[0]['validate']('anotherCommand'));
-    }
-
-    public function testStartAction()
-    {
-        $commandMessage = $this->createMock('CharlotteDunois\Livia\CommandMessage');
-        $promise = new Promise(function () {
-        });
-        $commandMessage->expects($this->once())->method('say')->with('Давайте сыграем')->willReturn($promise);
-
-        $this->command->run($commandMessage, new ArrayObject(['action' => 'start']), false);
-    }
-
-    public function testAddAction()
-    {
-        $commandMessage = $this->createMock('CharlotteDunois\Livia\CommandMessage');
-        $promise = new Promise(function () {
-        });
-        $commandMessage->expects($this->once())->method('say')->with('Добавьте новый вопрос')->willReturn($promise);
-
-        $this->command->run($commandMessage, new ArrayObject(['action' => 'add']), false);
-    }
-
     public function testSimpleResponseToTheDiscord(): void
     {
+        $result = ['question' => 'Вопрос', 'answer' => 'Ответ', 'tags' => 'Теги'];
+        $expectedResult = 'Вопрос';
+
         $commandMessage = $this->createMock('CharlotteDunois\Livia\CommandMessage');
         $promise = new Promise(function () {
         });
 
-        $commandMessage->expects($this->once())->method('say')->with('quiz [start|add]')->willReturn($promise);
+        $commandMessage->expects($this->once())->method('say')->with($expectedResult)->willReturn($promise);
+
+        $questionStore = $this->getMockBuilder('QuizStore')->setMethods(['get', 'load'])->getMock();
+        $questionStore->expects($this->once())->method('get')->willReturn($result);
+
+        // Подменяем store через reflection
+        $reflection = new \ReflectionClass($this->command);
+        $storeProperty = $reflection->getProperty('store');
+        $storeProperty->setAccessible(true);
+        $storeProperty->setValue($this->command, $questionStore);
 
         $this->command->run($commandMessage, new ArrayObject(), false);
     }
