d0f339d40676bd24cb0143664a31c56007c32642
Ivan Kuznetsov <Dizzy1986@yandex.ru>
2019-03-07 15:46:40 +0300

    Implemented user entity and tests

--- commands/Leaderboard/Implementations/User.php
+++ commands/Leaderboard/Implementations/User.php
@@ -4,6 +4,39 @@ namespace SoerBot\Commands\Leaderboard\Implementations;
 
 class User
 {
-    public $name;
-    public $rewards;
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var Reward[]
+     */
+    protected $rewards;
+
+    /**
+     * @var string
+     */
+    protected $linesDelimiter;
+
+    public function __construct($name, array $rewards, $linesDelimiter = PHP_EOL)
+    {
+        $this->name = $name;
+        $this->linesDelimiter = $linesDelimiter;
+
+        foreach ($rewards as $reward) {
+            $this->rewards[] = new Reward($reward['emoji'], $reward['count']);
+        }
+    }
+
+    public function __toString()
+    {
+        $rewards = '';
+
+        foreach ($this->rewards as $reward) {
+            $rewards .= $reward . $this->linesDelimiter;
+        }
+
+        return '@' . $this->name . $this->linesDelimiter . $rewards;
+    }
 }
