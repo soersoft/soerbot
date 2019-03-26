afe2f9aeb2b04bf734852559af79b392aecc3ff2
DenisGeek <spam@denis.tk>
2019-03-26 16:06:49 +0100

    namespace problem same directive in runner.php works well use React\EventLoop\Factory; $loop = Factory::create();

--- src/WebServer/WebHookServerResearch.php
+++ src/WebServer/WebHookServerResearch.php
@@ -1,61 +1,59 @@
 <?php
+    // use \Psr\Http\Message\ServerRequestInterface;
+    // use \React\Http\Response;
+    // use React\Http\Server;
+    use React\EventLoop\Factory;
+namespace SoerBot\WebServer;
+    
+class WebHookServerResearch
+{
+    private $loop;
+    private $server;
+    private $socket;
 
-use Psr\Http\Message\ServerRequestInterface;
-use React\Http\Response;
-use React\Http\Server;
-
-namespace SoerBot\WebServer{
-
-    class WebHookServerResearch
+    public function __construct()
     {
-        private $loop;
-        private $server;
-        private $socket;
+        echo "WebHookServerResearch has created\n";
+    }
+    
 
-        public function __construct()
-        {
-            echo "WebHookServerResearch has created";
+    public function StartServer()
+    {
+        // https://reactphp.org/
+        // see example api: https://habr.com/ru/post/143317/
+        // $this -> loop = Factory::create();
 
-        }
-        
+        // $this -> server = new Server(function (ServerRequestInterface $request) {
+        //     return new Response(
+        //         200,
+        //         array('Content-Type' => 'text/plain'),
+        //         "Hello World!\n"
+        //     );
+        // });
 
-        public function StartServer()
-        {
-            // https://reactphp.org/
-            // $this -> loop = React\EventLoop\Factory::create();
+        // $this -> socket = new Server(8080,  $this -> loop);
+        // $this -> server->listen( $this -> socket);
 
-            // $this -> server = new Server(function (ServerRequestInterface $request) {
-            //     return new Response(
-            //         200,
-            //         array('Content-Type' => 'text/plain'),
-            //         "Hello World!\n"
-            //     );
-            // });
+        // echo "Server running at http://127.0.0.1:8080\n";
 
-            // $this -> socket = new Server(8080,  $this -> loop);
-            // $this -> server->listen( $this -> socket);
+        // $this -> loop->run();
 
-            // echo "Server running at http://127.0.0.1:8080\n";
+        $loop = Factory::create();
+        // $loop = React\EventLoop\Factory::create();
 
-            // $this -> loop->run();
+        $server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) {
+            return new React\Http\Response(
+                200,
+                array('Content-Type' => 'text/plain'),
+                "Hello World!\n"
+            );
+        });
 
-            $loop = React\EventLoop\Factory::create();
+        $socket = new React\Socket\Server(8080, $loop);
+        $server->listen($socket);
 
-            $server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) {
-                return new React\Http\Response(
-                    200,
-                    array('Content-Type' => 'text/plain'),
-                    "Hello World!\n"
-                );
-            });
+        echo "Server running at http://127.0.0.1:8080\n";
 
-            $socket = new React\Socket\Server(8080, $loop);
-            $server->listen($socket);
-
-            echo "Server running at http://127.0.0.1:8080\n";
-
-            $loop->run();
-        }
+        $loop->run();
     }
 }
-?>
