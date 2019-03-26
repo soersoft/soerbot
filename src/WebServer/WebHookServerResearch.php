f2b93cf8baf2518ff793faf35a1f2539d4c0c08e
DenisGeek <spam@denis.tk>
2019-03-26 19:01:31 +0100

    Server constructor isn't working, no clue where from variable $request

--- src/WebServer/WebHookServerResearch.php
+++ src/WebServer/WebHookServerResearch.php
@@ -1,8 +1,10 @@
 <?php
 namespace SoerBot\WebServer;
-    // use \Psr\Http\Message\ServerRequestInterface;
-    // use \React\Http\Response;
-    // use React\Http\Server;
+    //composer require react/http:^0.8.1
+    //composer require react/socket:^1.2
+    use Psr\Http\Message\ServerRequestInterface;
+    use React\Http\Response;
+    use React\Http\Server;
     use React\EventLoop\Factory;
 
     
@@ -14,6 +16,7 @@ class WebHookServerResearch
 
     public function __construct()
     {
+        // echo phpinfo();
         echo "WebHookServerResearch has created\n";
     }
     
@@ -22,39 +25,21 @@ class WebHookServerResearch
     {
         // https://reactphp.org/
         // see example api: https://habr.com/ru/post/143317/
-        // $this -> loop = Factory::create();
+        $this -> loop = Factory::create();
 
-        // $this -> server = new Server(function (ServerRequestInterface $request) {
-        //     return new Response(
-        //         200,
-        //         array('Content-Type' => 'text/plain'),
-        //         "Hello World!\n"
-        //     );
-        // });
-
-        // $this -> socket = new Server(8080,  $this -> loop);
-        // $this -> server->listen( $this -> socket);
-
-        // echo "Server running at http://127.0.0.1:8080\n";
-
-        // $this -> loop->run();
-
-        $loop = Factory::create();
-        // $loop = React\EventLoop\Factory::create();
-
-        $server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) {
-            return new React\Http\Response(
+        $this -> server = new Server(function (ServerRequestInterface $request) {
+            return new Response(
                 200,
                 array('Content-Type' => 'text/plain'),
                 "Hello World!\n"
             );
         });
 
-        $socket = new React\Socket\Server(8080, $loop);
-        $server->listen($socket);
+        $this -> socket = new Server(8080,  $this -> loop);
+        $this -> server->listen( $this -> socket);
 
         echo "Server running at http://127.0.0.1:8080\n";
 
-        $loop->run();
+        $this -> loop->run();
     }
 }
