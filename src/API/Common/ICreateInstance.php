85d8c1e448c3245bc5596be43b6762193c81ad85
DenisGeek <spam@denis.tk>
2019-04-05 12:08:43 +0200

    API.Common has been added interfaces:  - ICreateInstance  - ITest

---
+++ src/API/Common/ICreateInstance.php
@@ -0,0 +1,7 @@
+<?php  
+namespace \API\Common;
+
+interface ICreateInstance
+{
+    function CreateInstance():object;
+}
---
+++ src/API/Common/ITest.php
@@ -0,0 +1,7 @@
+<?php  
+namespace \API\Common;
+
+interface ITest
+{
+    function Test():bool;
+}
