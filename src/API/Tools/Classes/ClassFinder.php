2949ea4720683fe8842c068d3e0f479db709c002
DenisGeek <spam@denis.tk>
2019-04-06 23:35:23 +0200

    API.Tools added IClassFinder and implementation in ClassFinder

---
+++ src/API/Tools/Classes/ClassFinder.php
@@ -0,0 +1,24 @@
+<?php  
+namespace \API\Tools;
+
+class ClassFinder implements IClassFinder
+{
+    /**
+     * finder all registred classes are implemens interfaceName
+     * thans to
+     * - https://stackoverflow.com/questions/3993759/php-how-to-get-a-list-of-classes-that-implement-certain-interface
+     * @property $interfaceName 
+     * - name of looking interface\class (IClassFinder::class)
+     * @return List<Type>
+     * - type implement or instance of interfaceName
+     */
+    function findClasses(string $interfaceName)
+    {
+        return array_filter(
+            get_declared_classes(),
+            function( $className ) use ( $interfaceName ) {
+                return in_array( $interfaceName, class_implements( $className ) );
+            }
+        );
+    }
+}
---
+++ src/API/Tools/Interfaces/IClassFinder.php
@@ -0,0 +1,13 @@
+<?php  
+namespace \API\Tools;
+
+interface IClassFinder
+{
+    /**
+     * @property $interfaceName 
+     * - name of looking interface\class (IClassFinder::class)
+     * @return List<Type>
+     * - type implement or instance of interfaceName
+     */
+    function findClasses(string $interfaceName);
+}
