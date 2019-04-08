edd8443f66497ac78b94138c2f053ce6f67bc8f3
DenisGeek <spam@denis.tk>
2019-04-08 20:29:58 +0200

    API.Common added IFactory

---
+++ src/API/Common/IFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace \API\Common;
+
+interface IFactory
+{
+    /**
+     * get all classes, implements
+     * - IPostSender
+     */
+    public function scan():array;
+    /**
+     * createIntances of all found casses 
+     */
+    public function createIntances(array $classes):array;
+}
