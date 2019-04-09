70b23facd954f410ca85abe5ef79e8e54ba28a88
DenisGeek <spam@denis.tk>
2019-04-09 13:52:54 +0200

    API.Send.MailSenderLight started

---
+++ src/API/Send/ClassesExample/MailSenderLight.php
@@ -0,0 +1,24 @@
+<?php
+
+use \API\Common;
+use \API\Tools;
+
+namespace \API\Send;
+/**
+ * supported interfaces:
+ * - API.SendIMailSender
+ *  - API.Common.ICreateInstance
+ */
+class MailSenderLight extends AMailSender
+{
+    /**
+     * implements:
+     * - API.Send.IMailSender
+     * 
+     * @return IMail
+     */
+    public function createMessage():IMail
+    {
+        $mail = new Mail();
+    }
+}
