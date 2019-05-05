76df5a840e76dfe3aca5dc7f336c0204ea0faa89
Ivan Kuznetsov <Dizzy1986@yandex.ru>
2019-05-05 22:30:52 +0300

    Implemented advanced command $leaderboard

---
+++ commands/Leaderboard/AdvImplementations/LeaderboardCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace SoerBot\Commands\Leaderboard\AdvImplementations;
+
+use CharlotteDunois\Livia\LiviaClient;
+use CharlotteDunois\Livia\CommandMessage;
+use CharlotteDunois\Livia\Commands\Command;
+
+class LeaderboardCommand extends Command
+{
+    const LEADERBOARD_IS_EMPTY = 'Пока в таблице лидеров никого нет.';
+    const EMOJI_OF_PRIZE_PLACES = [':one: ', ':two: ', ':three: '];
+
+    public function __construct(LiviaClient $client)
+    {
+        parent::__construct($client, [
+          'name' => 'leaderboard', // Give command name
+          'group' => 'utils', // Group in ['command', 'util']
+          'description' => 'Выводит таблицу участников и набранные очки', // Fill the description
+          'guildOnly' => false,
+          'throttling' => [
+            'usages' => 5,
+            'duration' => 10,
+          ],
+          'args' => [],
+        ]);
+    }
+
+    /**
+     * @param CommandMessage $message
+     * @param \ArrayObject $args
+     * @param bool $fromPattern
+     * @return \React\Promise\ExtendedPromiseInterface
+     */
+    public function run(CommandMessage $message, \ArrayObject $args, bool $fromPattern)
+    {
+        $users = new UsersModel(new LeaderBoardStoreJSONFile(__DIR__ . '/../Store/leaderboard.json'));
+        $users->load();
+
+        if ($users->all()->count() === 0) {
+            return $message->say(self::LEADERBOARD_IS_EMPTY);
+        }
+
+        $leaderboard = $users
+          ->all()
+          ->sort(function (User $a, User $b) {
+              return $b->getPointsAmount() - $a->getPointsAmount();
+          })->map(function (User $user) {
+              return $user->toString();
+          });
+
+        return $message->say(implode($leaderboard->all()));
+    }
+}
